dbe79da59b6095233edee7c81ef5b01f0ca3f403
Ni-Sun <127489363+Ni-Sun@users.noreply.github.com>
2025-06-11 23:55:44 +0800

    更新多个源文件的路径，修正stdint.h、stdio.h、stdlib.h和string.h的引用路径为Keil安装目录，删除不再使用的malloc和wifidriver文件，优化项目配置文件。

--- OBJ/main.d
+++ OBJ/main.d
@@ -1,13 +1,13 @@
 ..\obj\main.o: main.c
-..\obj\main.o: D:\ARM\ARMCC\Bin\..\include\stdio.h
-..\obj\main.o: D:\ARM\ARMCC\Bin\..\include\stdlib.h
-..\obj\main.o: D:\ARM\ARMCC\Bin\..\include\string.h
+..\obj\main.o: D:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+..\obj\main.o: D:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdlib.h
+..\obj\main.o: D:\Program Files\Keil\ARM\ARMCC\Bin\..\include\string.h
 ..\obj\main.o: hanzi.h
 ..\obj\main.o: ..\HARDWARE\LED\led.h
 ..\obj\main.o: ..\SYSTEM\sys\sys.h
 ..\obj\main.o: ..\USER\stm32f10x.h
 ..\obj\main.o: ..\CORE\core_cm3.h
-..\obj\main.o: D:\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\main.o: D:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdint.h
 ..\obj\main.o: ..\USER\system_stm32f10x.h
 ..\obj\main.o: ..\USER\stm32f10x_conf.h
 ..\obj\main.o: ..\STM32F10x_FWLib\inc\stm32f10x_adc.h
@@ -45,3 +45,4 @@
 ..\obj\main.o: ..\HARDWARE\TOUCH\ott2001a.h
 ..\obj\main.o: ..\HARDWARE\TOUCH\gt9147.h
 ..\obj\main.o: ..\HARDWARE\TOUCH\ft5206.h
+..\obj\main.o: ..\HARDWARE\SPI\spi.h
